ab2a9ff5f4489f64c35b246478a35efdc47384dd
semual <semuall0108@gmail.com>
2024-02-25 22:16:30 -0800

    push

--- src/page/Resume.jsx
+++ src/page/Resume.jsx
@@ -20,7 +20,7 @@ export const Resume = (props) => {
                 </motion.p>
             </div>
             <motion.div className='resume-container' exit={{opacity: 0}}>
-                <span className='space-small'></span>
+                
                 <motion.h1 initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: "0.5", delay:0.25}}>Resume</motion.h1>
             </motion.div>
         
